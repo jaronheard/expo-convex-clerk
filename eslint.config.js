a5ddbe1a331e06f033b0ab251bef14126d73b56d
Jaron Heard <jaronheard@gmail.com>
2025-05-21 12:20:34 -0700

    set up prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const reactCompilerPlugin = require("eslint-plugin-react-compiler");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
     plugins: {
       "react-compiler": reactCompilerPlugin,
