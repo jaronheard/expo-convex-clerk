a7dbaf0994a5d8818fbe3bc2d46e19efb3043eeb
Jaron Heard <jaronheard@gmail.com>
2025-05-21 16:40:45 -0700

    feat: enable query caching

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,8 +2,8 @@ import {
   Authenticated,
   Unauthenticated,
   useConvexAuth,
-  useQuery,
 } from "convex/react";
+import { useQuery } from "convex-helpers/react/cache";
 import { Redirect, Stack } from "expo-router";
 import React from "react";
 import { ActivityIndicator, StyleSheet, View } from "react-native";
